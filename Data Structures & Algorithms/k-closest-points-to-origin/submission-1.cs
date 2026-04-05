98f59d0345f15d9bcaf684a2236f490bd0102ebc
Hrithik Kumar <HrithikKumarce2021@gmail.com>
2026-04-05 20:20:39 +0530

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int[][] KClosest(int[][] points, int k) {
+        var pq = new PriorityQueue<int[], double>();
+        int[][] ans = new int[k][]; 
+
+        foreach(int[] point in points){
+            int x1 = point[0];
+            int y1 = point[1];
+
+            double distance = Math.Sqrt(Math.Pow((x1), 2) + Math.Pow(y1, 2));
+            pq.Enqueue(point, -distance);
+        }
+
+
+
+        for(int i = 0; i<points.Length-k; i++){
+            pq.Dequeue();
+
+
+        }
+
+
+        for(int i = 0; i<ans.Length; i++){
+            ans[i] = pq.Dequeue();
+        }
+
+        return ans;
+    }
+}
