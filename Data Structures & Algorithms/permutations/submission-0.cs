f9b4df98963887e79de613acb5c5f57c98bc5367
Hrithik Kumar <HrithikKumarce2021@gmail.com>
2026-04-10 21:26:01 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public List<List<int>> result;
+    public List<List<int>> Permute(int[] nums) {
+        result = new List<List<int>>();
+
+        bool[] used = new bool[nums.Length];
+
+        BackTrack(nums, new List<int>(), used);
+
+        return result;
+    }
+
+    public void BackTrack(int[] nums, List<int> res, bool[] used){
+        if(res.Count == nums.Length){
+            result.Add(new List<int>(res));
+            return;
+        }
+
+        for(int i = 0; i<nums.Length; i++){
+            if(used[i]) continue;
+
+            //choose
+            res.Add(nums[i]);
+            used[i] = true;
+
+            BackTrack(nums, res, used);
+
+            //remove 
+
+            res.RemoveAt(res.Count - 1);
+            used[i] = false;
+        }
+    }
+}
