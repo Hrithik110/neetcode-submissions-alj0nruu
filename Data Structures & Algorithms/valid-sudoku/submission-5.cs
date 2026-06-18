beeb5f693bfd07f694f0b0da98fdc746abea013a
Hrithik Kumar <HrithikKumarce2021@gmail.com>
2026-06-18 20:29:34 +0530

    Add: valid-sudoku - submission-5

---
+++ Data Structures & Algorithms/valid-sudoku/submission-5.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public bool IsValidSudoku(char[][] board) {
+        // var rows = new HashSet<char>[9];
+        // var cols = new HashSet<char>[9];
+        // var boxes = new HashSet<char>[9];
+
+        //  for(int i = 0; i<9; i++){
+        //     rows[i] = new HashSet<char>();
+        //     cols[i] = new HashSet<char>();
+        //     boxes[i] = new HashSet<char>();
+        // }
+
+        var rows = Enumerable.Range(0, 9).Select(_ => new HashSet<char>()).ToArray();
+        var cols = Enumerable.Range(0, 9).Select(_ => new HashSet<char>()).ToArray();
+        var boxes = Enumerable.Range(0, 9).Select(_ => new HashSet<char>()).ToArray();
+
+        for(int i = 0; i<board.Length; i++){
+
+            for(int j = 0; j<board[0].Length; j++){
+
+                char ch = board[i][j];
+
+                if(ch == '.') continue;
+
+                int boxIdx = (i/3) + (j/3)*3;
+
+                if(rows[i].Contains(ch) || cols[j].Contains(ch) || boxes[boxIdx].Contains(ch)){
+                    return false;
+                }
+                rows[i].Add(ch);
+                cols[j].Add(ch);
+                boxes[boxIdx].Add(ch);
+            }
+        }
+
+        return true;
+
+    }
+}
