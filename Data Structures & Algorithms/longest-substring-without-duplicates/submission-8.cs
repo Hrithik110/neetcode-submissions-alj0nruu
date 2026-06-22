0e41be65978d10dde192d94242bcc660d0afd23a
Hrithik Kumar <HrithikKumarce2021@gmail.com>
2026-06-22 20:28:58 +0530

    Update: longest-substring-without-duplicates - submission-8

--- Data Structures & Algorithms/longest-substring-without-duplicates/submission-8.cs
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-8.cs
@@ -1,19 +1,18 @@
 public class Solution {
     public int LengthOfLongestSubstring(string s) {
-        var charIdxMap = new Dictionary<char, int>();
+        var charMap = new Dictionary<char, int>();
 
-        int longestSequence = 0;
+        int longestSeq = 0;
         int start = 0;
-
         for(int i = 0; i<s.Length; i++){
-            if(charIdxMap.ContainsKey(s[i]) && charIdxMap[s[i]]>= start){
-                start = charIdxMap[s[i]] + 1;
+            if(charMap.ContainsKey(s[i]) && charMap[s[i]] >= start){
+                start = charMap[s[i]] + 1;
             }
-            charIdxMap[s[i]] = i;
+            charMap[s[i]] = i;
 
-            longestSequence = Math.Max(longestSequence, i-start+1);
+            longestSeq = Math.Max(longestSeq, i - start + 1);
         }
 
-        return longestSequence;
+        return longestSeq;
     }
 }
